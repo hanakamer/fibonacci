e19267c0e40a98708cac179fec2049acd06c87da
hanakamer <hanakamer@gmail.com>
2016-07-26 09:14:10 +0300

    add timing function

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,5 @@
 import time
+import timeit
 class Fibonacci:
     # her sayi icin oncesindeki sayilari tekrar tekrar heaplamasi gerekiyor
     # fib(5) =
@@ -13,62 +14,49 @@ class Fibonacci:
     def recursive_fib(self,n):
         if n < 0:
             return 'negatif sayi giremezsiniz'
-        else:
-            if n < 2:
-                return n
-            return self.recursive_fib(n-1) + self.recursive_fib(n-2)
+        if n < 2:
+            return n
+        return self.recursive_fib(n-1) + self.recursive_fib(n-2)
+
 
     # asagidan  yukari ilerleyerek liste olusturuyoruz
     # boylece onceden hesaplanan sayi daha sonra tekar kullanilabilir
     def bottomup_fib(self,n):
         if n < 0:
             return 'negatif sayi giremezsiniz'
-        else:
-            if n == 0:
-                return 0
-            fibo = [0,1]
-            for i in range(2, n+1):
-                fibo.append(fibo[-1] + fibo[-2])
-            stop_time_bottomup = time.time()
-            return fibo
+        if n == 0:
+            return 0
+        fibo = [0,1]
+        for i in range(2, n+1):
+            fibo.append(fibo[-1] + fibo[-2])
+        stop_time_bottomup = time.time()
+        return fibo
+
 
     def iterative_fib(self,n):
         if n < 0:
             return 'negatif sayi giremezsiniz'
-        else:
-            a = 0
-            b = 1
-            for i in range (1,n + 1):
-                temp = a
-                a = b
-                b += temp
-            return a
+        a = 0
+        b = 1
+        for i in range (1,n + 1):
+            temp = a
+            a = b
+            b += temp
+        return a
 
-            
+def timing(func, args):
+    time_start = time.time()
+    ret = func(args)
+    time_stop = time.time()
+    print '------ %s function took %0.3f ms ------' % (func.func_name, (time_stop-time_start)*1000.0)
+    return ret
+
 ##############
 # You can uncomment these lines to compare the methods and their performances
 ##############
 
 # fibo = Fibonacci()
 
-# start_time_reccursive = time.time()
-# recursive_result = fibo.recursive_fib(5)
-# stop_time_reccursive = time.time()
-#
-# recursive_time = stop_time_reccursive - start_time_reccursive
-#
-# start_time_bottomup = time.time()
-# bottomup_result = fibo.bottomup_fib(5)
-# stop_time_bottomup = time.time()
-#
-# bottomup_time = stop_time_bottomup - start_time_bottomup
-#
-# start_time_iterative = time.time()
-# iterative_result = fibo.iterative_fib(5)
-# stop_time_iterative = time.time()
-#
-# iterative_time = stop_time_iterative - start_time_iterative
-
-# print("--- %s seconds --- recursive" % (recursive_time))
-# print("--- %s seconds --- bottomup" % (bottomup_time))
-# print("--- %s seconds --- iterative" % (iterative_time))
+# timing(fibo.recursive_fib,5)
+# timing(fibo.bottomup_fib,5)
+# timing(fibo.iterative_fib,5)
